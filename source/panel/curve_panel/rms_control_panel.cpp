73d6e7395b1c3e82e260feb82bb19019220a2602
zsliu98 <zsliu98@gmail.com>
2025-11-15 23:19:38 -0500

    fix(rms panel): adjust layout

--- source/panel/curve_panel/rms_control_panel.cpp
+++ source/panel/curve_panel/rms_control_panel.cpp
@@ -10,8 +10,8 @@
 #include "rms_control_panel.hpp"
 
 namespace zlpanel {
-    RMSControlPanel::RMSControlPanel(PluginProcessor &p, zlgui::UIBase &base,
-                                     const multilingual::TooltipHelper &tooltip_helper)
+    RMSControlPanel::RMSControlPanel(PluginProcessor& p, zlgui::UIBase& base,
+                                     const multilingual::TooltipHelper& tooltip_helper)
         : base_(base), updater_(),
           rms_show_ref_(*p.na_parameters_.getRawParameterValue(zlstate::PRMSPanelDisplay::kID)),
           label_laf_(base),
@@ -31,7 +31,7 @@ namespace zlpanel {
           rms_mix_attachment_(rms_mix_slider_.getSlider(), p.parameters_,
                               zlp::PRMSMix::kID, updater_) {
         label_laf_.setFontScale(1.25f);
-        for (auto &l: {&rms_length_label_, &rms_speed_label_, &rms_mix_label_}) {
+        for (auto& l : {&rms_length_label_, &rms_speed_label_, &rms_mix_label_}) {
             l->setLookAndFeel(&label_laf_);
             l->setInterceptsMouseClicks(false, false);
             l->setJustificationType(juce::Justification::centredLeft);
@@ -39,7 +39,7 @@ namespace zlpanel {
             addAndMakeVisible(l);
         }
 
-        for (auto &s: {&rms_length_slider_, &rms_speed_slider_, &rms_mix_slider_}) {
+        for (auto& s : {&rms_length_slider_, &rms_speed_slider_, &rms_mix_slider_}) {
             s->setFontScale(1.25f);
             s->getSlider().setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
             s->getSlider().setSliderSnapsToMousePosition(false);
@@ -48,7 +48,7 @@ namespace zlpanel {
         }
     }
 
-    void RMSControlPanel::paint(juce::Graphics &g) {
+    void RMSControlPanel::paint(juce::Graphics& g) {
         g.fillAll(base_.getBackgroundColour());
     }
 
@@ -59,29 +59,31 @@ namespace zlpanel {
 
     int RMSControlPanel::getIdealHeight() const {
         const auto padding = juce::roundToInt(base_.getFontSize() * kPaddingScale);
-        const auto button_height = juce::roundToInt(base_.getFontSize() * kButtonScale);
-        return padding * 4 + button_height * 3;
+        const auto c_height = juce::roundToInt(base_.getFontSize() * 1.75f);
+        return c_height * 3 + (padding / 2) * 2;
     }
 
     void RMSControlPanel::resized() {
         const auto padding = juce::roundToInt(base_.getFontSize() * kPaddingScale);
-        const auto button_height = juce::roundToInt(base_.getFontSize() * kButtonScale);
+        const auto c_height = juce::roundToInt(base_.getFontSize() * 1.75f);
         const auto slider_width = juce::roundToInt(base_.getFontSize() * kSliderScale);
 
         auto bound = getLocalBounds();
         bound.removeFromLeft(padding);
-        bound.removeFromTop(padding); {
-            auto t_bound = bound.removeFromTop(button_height);
+        {
+            auto t_bound = bound.removeFromTop(c_height);
             rms_length_label_.setBounds(t_bound.removeFromLeft(slider_width));
             rms_length_slider_.setBounds(t_bound);
         }
-        bound.removeFromTop(padding); {
-            auto t_bound = bound.removeFromTop(button_height);
+        bound.removeFromTop(padding / 2);
+        {
+            auto t_bound = bound.removeFromTop(c_height);
             rms_speed_label_.setBounds(t_bound.removeFromLeft(slider_width));
             rms_speed_slider_.setBounds(t_bound);
         }
-        bound.removeFromTop(padding); {
-            auto t_bound = bound.removeFromTop(button_height);
+        bound.removeFromTop(padding / 2);
+        {
+            auto t_bound = bound.removeFromTop(c_height);
             rms_mix_label_.setBounds(t_bound.removeFromLeft(slider_width));
             rms_mix_slider_.setBounds(t_bound);
         }
